b7759ef55385930291c25e7c61d1370927f8121e
Doroga80 <rusdorog@yandex.by>
2023-12-03 14:00:49 +0300

    new fule

---
+++ homeWork6/data.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+      "id": 1,
+      "name": "Product 1",
+      "image": "product1.jpg",
+      "price": "$100",
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      "id": 2,
+      "name": "Product 2",
+      "image": "product2.jpg",
+      "price": "$150",
+      "description": "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+      "id": 3,
+      "name": "Product 3",
+      "image": "product3.jpg",
+      "price": "$120",
+      "description": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    }
+  ];
+  
+  export default data;
+  
